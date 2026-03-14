fb22f4f241f6c7e5505531c1b4207ac56ac81ea9
The_Meme_Gaimer <therealvalejuve1708@gmail.com>
2026-03-14 15:19:07 +0100

    Add mini-game: Guess the number feature

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
+#include <cstdlib>   // per rand() e srand()
+#include <ctime>     // per time()
 using namespace std;
 
 int main() {
     int scelta = 0;
+
+    // inizializza il generatore di numeri casuali
+    srand(time(0));
 
     do {
         cout << "\n=== MENU PRINCIPALE ===\n";
         cout << "1) Saluta\n";
         cout << "2) Mostra un numero\n";
         cout << "3) Esci\n";
+        cout << "4) Mini-gioco: Indovina il numero\n";
         cout << "Scegli un'opzione: ";
         cin >> scelta;
 
@@ -24,6 +30,31 @@ int main() {
             case 3:
                 cout << "\nUscita dal programma...\n";
                 break;
+
+            case 4: {
+                cout << "\n=== INDOVINA IL NUMERO ===\n";
+                int numeroSegreto = rand() % 50 + 1; // numero tra 1 e 50
+                int tentativo = 0;
+                int tentativi = 0;
+
+                cout << "Ho scelto un numero tra 1 e 50. Prova a indovinarlo!\n";
+
+                do {
+                    cout << "Il tuo tentativo: ";
+                    cin >> tentativo;
+                    tentativi++;
+
+                    if (tentativo > numeroSegreto)
+                        cout << "Troppo alto!\n";
+                    else if (tentativo < numeroSegreto)
+                        cout << "Troppo basso!\n";
+                    else
+                        cout << "Bravo Valentino! Hai indovinato in " << tentativi << " tentativi!\n";
+
+                } while (tentativo != numeroSegreto);
+
+                break;
+            }
 
             default:
                 cout << "\nScelta non valida. Riprova.\n";
