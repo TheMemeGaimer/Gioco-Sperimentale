ac852062780cec4ad411810e4720cb4ddede2ddc
The_Meme_Gaimer <therealvalejuve1708@gmail.com>
2026-03-14 15:10:42 +0100

    Add main.cpp with welcome message in Italian

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Benvenuto nel progetto C++ di Valentino!" << endl;
+    cout << "Questa è la versione dentro la cartella src." << endl;
+    return 0;
+}
